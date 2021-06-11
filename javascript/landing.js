22f15c82542ea562898a6ae329afbb309c1129a4
Raj varsani <varasaniraj5@gmail.com>
2021-06-11 23:37:01 +0530

    done a few changes >> added escape footer hide feature >> added pointer cursor to arrow >> changed h1 > h2 in footer headings

--- javascript/landing.js
+++ javascript/landing.js
@@ -4,57 +4,68 @@ let main = document.getElementsByClassName("main");
 let down = true;
 let count = 0;
 arrow[0].addEventListener("click", () => {
-    main[0].classList.toggle("main-opacity");
-    count++;
-    if(count%4==1){
-        arrow[0].classList.toggle("scale");
-        arrow[0].classList.toggle("rotate");
-        arrow[0].classList.toggle("rotate2");
-    }
-    else if(count%4==2){
-        arrow[0].classList.toggle("rotate");
-        arrow[0].classList.toggle("rotate2");
-        setTimeout(()=>{
-            arrow[0].classList.toggle("scale");
-        },700)
-    }
-    else if(count%4==3){
-        arrow[0].classList.toggle("scale");
-        arrow[0].classList.toggle("rotate2");
-        arrow[0].classList.toggle("rotate");
-    }
-    else if(count%4==0){
-        arrow[0].classList.toggle("rotate");
-        arrow[0].classList.toggle("rotate2");
-        setTimeout(()=>{
-            arrow[0].classList.toggle("scale");
-        },700)
-    }
-    if (down) {
-        footer[0].classList.toggle("display-none");
-        let distance = 275;
-        let a = setInterval(() => {
-            console.log(distance);
-            distance = distance - 5;
-            scrollBy(0, 5);
-            console.log(distance);
-            if (distance <= 0) {
-                clearInterval(a);
-                down = false;
-            }
-        }, 8);
-    } else {
-        let upwards = 275;
-        let b = setInterval(() => {
-            console.log(upwards);
-            upwards = upwards - 5;
-            scrollBy(0, -5);
-            console.log(upwards);
-            if (upwards <= 0) {
-                clearInterval(b);
-                down = true;
-                footer[0].classList.toggle("display-none");
-            }
-        }, 8);
-    }
+  downArrowButtonTriggred();
 });
+
+window.onkeydown = function (e) {
+  if (e.keyCode === 27) {
+    // Key code for ESC key
+    e.preventDefault();
+    if (!down) {
+      downArrowButtonTriggred();
+    }
+  }
+};
+
+function downArrowButtonTriggred() {
+  main[0].classList.toggle("main-opacity");
+  count++;
+  if (count % 4 == 1) {
+    arrow[0].classList.toggle("scale");
+    arrow[0].classList.toggle("rotate");
+    arrow[0].classList.toggle("rotate2");
+  } else if (count % 4 == 2) {
+    arrow[0].classList.toggle("rotate");
+    arrow[0].classList.toggle("rotate2");
+    setTimeout(() => {
+      arrow[0].classList.toggle("scale");
+    }, 700);
+  } else if (count % 4 == 3) {
+    arrow[0].classList.toggle("scale");
+    arrow[0].classList.toggle("rotate2");
+    arrow[0].classList.toggle("rotate");
+  } else if (count % 4 == 0) {
+    arrow[0].classList.toggle("rotate");
+    arrow[0].classList.toggle("rotate2");
+    setTimeout(() => {
+      arrow[0].classList.toggle("scale");
+    }, 700);
+  }
+  if (down) {
+    footer[0].classList.toggle("display-none");
+    let distance = 275;
+    let a = setInterval(() => {
+      console.log(distance);
+      distance = distance - 5;
+      scrollBy(0, 5);
+      console.log(distance);
+      if (distance <= 0) {
+        clearInterval(a);
+        down = false;
+      }
+    }, 8);
+  } else {
+    let upwards = 275;
+    let b = setInterval(() => {
+      console.log(upwards);
+      upwards = upwards - 5;
+      scrollBy(0, -5);
+      console.log(upwards);
+      if (upwards <= 0) {
+        clearInterval(b);
+        down = true;
+        footer[0].classList.toggle("display-none");
+      }
+    }, 8);
+  }
+}
