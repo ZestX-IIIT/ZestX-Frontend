cf42ab064acd3f72de79a98dc9c4ba8db3ee73aa
Raj varsani <varasaniraj5@gmail.com>
2021-06-13 10:59:56 +0530

    removed count usage in footer toggle

--- javascript/landing.js
+++ javascript/landing.js
@@ -2,76 +2,73 @@ let footer = document.getElementsByClassName("footer");
 let arrow = document.getElementsByClassName("arrow");
 let main = document.getElementsByClassName("main");
 let down = true;
-let count = 0;
 arrow[0].addEventListener("click", () => {
-  downArrowButtonTriggred();
+  toggleFooterStates();
 });
 
 window.onkeydown = function (e) {
   if (e.keyCode === 27) {
     // ESC key trigger to collpase
     if (!down) {
-      downArrowButtonTriggred();
+      collapseFooter()
     }
   } else if (e.keyCode === 70) {
     // F key trigger to Expand/collpase both
-      downArrowButtonTriggred();
+   toggleFooterStates()
   }
 };
 
-function downArrowButtonTriggred() {
+
+function toggleFooterStates() {
   if (down) {
-    down = false;
+   expandFooter()
   } else {
-    down = true;
-  }
-  main[0].classList.toggle("main-opacity");
-  count++;
-  if (count % 4 == 1) {
-    arrow[0].classList.toggle("scale");
-    arrow[0].classList.toggle("rotate");
-    arrow[0].classList.toggle("rotate2");
-  } else if (count % 4 == 2) {
-    arrow[0].classList.toggle("rotate");
-    arrow[0].classList.toggle("rotate2");
-    setTimeout(() => {
-      arrow[0].classList.toggle("scale");
-    }, 700);
-  } else if (count % 4 == 3) {
-    arrow[0].classList.toggle("scale");
-    arrow[0].classList.toggle("rotate2");
-    arrow[0].classList.toggle("rotate");
-  } else if (count % 4 == 0) {
-    arrow[0].classList.toggle("rotate");
-    arrow[0].classList.toggle("rotate2");
-    setTimeout(() => {
-      arrow[0].classList.toggle("scale");
-    }, 700);
-  }
-  if (!down) {
-    footer[0].classList.toggle("display-none");
-    let distance = 275;
-    let a = setInterval(() => {
-      console.log(distance);
-      distance = distance - 5;
-      scrollBy(0, 5);
-      console.log(distance);
-      if (distance <= 0) {
-        clearInterval(a);
-      }
-    }, 8);
-  } else {
-    let upwards = 275;
-    let b = setInterval(() => {
-      console.log(upwards);
-      upwards = upwards - 5;
-      scrollBy(0, -5);
-      console.log(upwards);
-      if (upwards <= 0) {
-        clearInterval(b);
-        footer[0].classList.toggle("display-none");
-      }
-    }, 8);
+   collapseFooter()
   }
 }
-//Hello world
+
+
+function collapseFooter() {
+  down = true;
+
+  main[0].classList.toggle("main-opacity");  
+  arrow[0].classList.toggle("rotate2");
+  arrow[0].classList.toggle("rotate");
+  setTimeout(() => {
+    arrow[0].classList.toggle("scale");
+  }, 700);
+
+  let upwards = 275;
+  let b = setInterval(() => {
+    console.log(upwards);
+    upwards = upwards - 5;
+    scrollBy(0, -5);
+    console.log(upwards);
+    if (upwards <= 0) {
+      clearInterval(b);
+      footer[0].classList.toggle("display-none");
+    }
+  }, 8);
+
+}
+
+function expandFooter() {
+  down = false;
+
+  main[0].classList.toggle("main-opacity");
+  arrow[0].classList.toggle("rotate");
+  arrow[0].classList.toggle("rotate2");
+  arrow[0].classList.toggle("scale");
+
+  footer[0].classList.toggle("display-none");
+  let distance = 275;
+  let a = setInterval(() => {
+    console.log(distance);
+    distance = distance - 5;
+    scrollBy(0, 5);
+    console.log(distance);
+    if (distance <= 0) {
+      clearInterval(a);
+    }
+  }, 8);
+}
