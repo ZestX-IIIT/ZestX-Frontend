e4418c5d3d31507fb82ca661df3fc49c23c615c0
Akash Gupta <akash.gupta.anup@gmail.com>
2021-06-14 18:57:03 +0530

    slider Implemented

---
+++ java.js
@@ -0,0 +1,21 @@
+var slider = document.getElementsByClassName("slide")
+let index = 0;
+
+var a = setInterval(()=>{
+    slider[index].checked = true;
+    index++;
+    index = index%5;
+},500)
+var slide = document.getElementById("slider");
+slide.addEventListener('mouseover' , ()=>{
+    console.log('hello')
+    clearInterval(a)
+})
+
+slide.addEventListener('mouseout' , ()=>{
+    a = setInterval(()=>{
+        slider[index].checked = true;
+        index++;
+        index = index%5;
+    },1500)
+})
