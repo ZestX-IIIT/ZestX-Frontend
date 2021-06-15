c9b3a48a8b1451aff3b1a31888a0778423009a85
Raj varsani <varasaniraj5@gmail.com>
2021-06-15 14:08:16 +0530

    done a few changes >> added illustartion for home page animations >> slider  bux fix

--- java.js
+++ java.js
@@ -1,21 +1,22 @@
 var slider = document.getElementsByClassName("slide")
 let index = 0;
+var isNotHovering = true
 
-var a = setInterval(()=>{
-    slider[index].checked = true;
-    index++;
-    index = index%5;
-},2500)
-var slide = document.getElementById("slider");
-slide.addEventListener('mouseover' , ()=>{
-    console.log('hello')
-    clearInterval(a)
-})
-
-slide.addEventListener('mouseout' , ()=>{
-    a = setInterval(()=>{
+var a = setInterval(() => {
+    if (isNotHovering) {
         slider[index].checked = true;
         index++;
-        index = index%5;
-    },2500)
+        index = index % 5;
+    }
+}, 2500)
+
+
+var slide = document.getElementById("slider");
+slide.addEventListener('mouseover', () => {
+    console.log('hello')
+    isNotHovering = false
+})
+
+slide.addEventListener('mouseout', () => {
+    isNotHovering = true
 })
