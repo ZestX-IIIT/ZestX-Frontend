ae2e0a9c4d1bcddd2cf4637227659487590e5128
HarshilMendpara <mendparaharshil@gmail.com>
2021-06-18 19:51:31 +0530

    added animation in navigation bar

--- javascript/home.js
+++ javascript/home.js
@@ -7,8 +7,12 @@ let events = document.getElementById("events");
 let sponsers = document.getElementById("sponsers");
 let team = document.getElementById("team");
 let faq = document.getElementById("faq");
+let dot = document.getElementById("dot");
 let homeSection = document.getElementById("homesec");
 let eventsSection = document.getElementById("eventssec");
+let teamSection = document.getElementById("teamsec");
+let sponsersSection = document.getElementById("sponser_container");
+let faqSection = document.getElementById("faqsec");
 let index = 0;
 let activeTab = 0;
 
@@ -21,141 +25,105 @@ window.addEventListener("load", () => {
 window.addEventListener("scroll", () => {
   let h = window.innerHeight;
   if (window.pageYOffset <= h / 2) {
-    activeTab = 0;
-  } else if (window.pageYOffset <= (h * 3) / 2) {
-    activeTab = 1;
-  } else if (window.pageYOffset <= (h * 5) / 2) {
-    activeTab = 2;
-  } else if (window.pageYOffset <= (h * 7) / 2) {
-    activeTab = 3;
-  } else {
-    activeTab = 4;
-  }
-});
-
-var nav = document.getElementById("navelements");
-var line = $("<div />").addClass("line");
-
-line.appendTo(nav);
-
-var active = nav.find(".active");
-var pos = 0;
-var wid = 0;
-
-if (active.length) {
-  pos = active.position().left;
-  wid = active.width();
-  line.css({
-    left: pos,
-    width: wid,
-  });
-}
-
-nav.find("ul li a").click(function (e) {
-  e.preventDefault();
-  if (!$(this).parent().hasClass("active") && !nav.hasClass("animate")) {
-    nav.addClass("animate");
-
-    var _this = $(this);
-
-    nav.find("ul li").removeClass("active");
-
-    var position = _this.parent().position();
-    var width = _this.parent().width();
-
-    if (position.left >= pos) {
-      line.animate(
-        {
-          width: position.left - pos + width,
-        },
-        300,
-        function () {
-          line.animate(
-            {
-              width: width,
-              left: position.left,
-            },
-            150,
-            function () {
-              nav.removeClass("animate");
-            }
-          );
-          _this.parent().addClass("active");
-        }
-      );
-    } else {
-      line.animate(
-        {
-          left: position.left,
-          width: pos - position.left + wid,
-        },
-        300,
-        function () {
-          line.animate(
-            {
-              width: width,
-            },
-            150,
-            function () {
-              nav.removeClass("animate");
-            }
-          );
-          _this.parent().addClass("active");
-        }
-      );
+    if (activeTab != 0) {
+      activeTab = 0;
+      setActive(0);
+      deActive(1);
     }
-
-    pos = position.left;
-    wid = width;
+  } else if (window.pageYOffset <= (h * 3) / 2) {
+    if (activeTab != 1) {
+      activeTab = 1;
+      setActive(activeTab);
+      deActive(0);
+      deActive(2);
+    }
+  } else if (window.pageYOffset <= (h * 5) / 2) {
+    if (activeTab != 2) {
+      activeTab = 2;
+      setActive(activeTab);
+      deActive(1);
+      deActive(3);
+    }
+  } else if (window.pageYOffset <= (h * 7) / 2) {
+    if (activeTab != 3) {
+      activeTab = 3;
+      setActive(activeTab);
+      deActive(2);
+      deActive(4);
+    }
+  } else {
+    if (activeTab != 4) {
+      activeTab = 4;
+      setActive(activeTab);
+      deActive(3);
+    }
   }
 });
 
-home.addEventListener("click", () => {});
-
-events.addEventListener("click", () => {});
-team.addEventListener("click", () => {});
-sponsers.addEventListener("click", () => {});
-faq.addEventListener("click", () => {});
-
-$('a[href^="#"]').on("click", function (e) {
-  // e.preventDefault();
-
-  var target = this.hash,
-    $target = $(target);
-
-  $("html, body")
-    .stop()
-    .animate(
-      {
-        scrollTop: $target.offset().top - 70,
-      },
-      900,
-      "swing",
-      function () {
-        window.location.hash = target;
-      }
-    );
+home.addEventListener("click", () => {
+  window.scrollTo(0, homeSection.offsetTop - 125);
+});
+events.addEventListener("click", () => {
+  window.scrollTo(0, eventsSection.offsetTop - 50);
+});
+team.addEventListener("click", () => {
+  window.scrollTo(0, teamSection.offsetTop);
+});
+sponsers.addEventListener("click", () => {
+  window.scrollTo(0, sponsersSection.offsetTop - 150);
+});
+faq.addEventListener("click", () => {
+  window.scrollTo(0, faqSection.offsetTop);
 });
 
-function toggleActiveTab(tabIndex) {
-  switch (activeTab) {
+function deActive(tabIndex) {
+  switch (tabIndex) {
     case 0:
-      home.classList.toggle("navactive");
+      dot.classList.remove("dothome");
+      home.classList.remove("navactive");
       break;
     case 1:
-      events.classList.toggle("navactive");
+      dot.classList.remove("dotevents");
+      events.classList.remove("navactive");
       break;
     case 2:
-      team.classList.toggle("navactive");
+      dot.classList.remove("dotteam");
+      team.classList.remove("navactive");
 
       break;
     case 3:
-      sponsers.classList.toggle("navactive");
+      dot.classList.remove("dotsponsers");
+      sponsers.classList.remove("navactive");
 
       break;
     case 4:
-      faq.classList.toggle("navactive");
+      dot.classList.remove("dotfaq");
+      faq.classList.remove("navactive");
 
+      break;
+  }
+}
+function setActive(tabIndex) {
+  switch (tabIndex) {
+    case 0:
+      home.classList.add("navactive");
+      dot.classList.add("dothome");
+      break;
+    case 1:
+      events.classList.add("navactive");
+      dot.classList.add("dotevents");
+      break;
+    case 2:
+      team.classList.add("navactive");
+      dot.classList.add("dotteam");
+      break;
+    case 3:
+      sponsers.classList.add("navactive");
+      dot.classList.add("dotsponsers");
+      break;
+    case 4:
+      faq.classList.add("navactive");
+      dot.classList.add("dotfaq");
       break;
   }
 }
