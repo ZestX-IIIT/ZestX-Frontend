5ae9ede6a9310bcb2d537aaba06f9dcc980fefb3
Akash Gupta <akash.gupta.anup@gmail.com>
2021-06-18 16:53:42 +0530

    footer completed
    implemented proper animations
    made outer div container as well

--- javascript/faq.js
+++ javascript/faq.js
@@ -1,10 +1,14 @@
-let faq_down_arrow = document.getElementById("faq_down_arrow");
+let faq_down_arrow = document.getElementsByClassName("faq_down_arrow");
 let FAQQuestionsContainer = document.getElementsByClassName("FAQQuestionsContainer");
 let AnswerOfQuestion = document.getElementsByClassName("AnswerOfQuestion");
 let arrowDiv = document.getElementsByClassName("arrowDiv");
+let FAQOuterContainer = document.getElementsByClassName("FAQOuterContainer");
 
-faq_down_arrow.addEventListener("click" , ()=>{
-    FAQQuestionsContainer[0].classList.toggle("FAQQuestionsContainerHeight")
-    AnswerOfQuestion[0].classList.toggle("AnswerOfQuestionOpacity")
-    arrowDiv[0].classList.toggle("FAQQuestionsContainerHeight")
+for(let i=0; i<faq_down_arrow.length;i++){
+faq_down_arrow[i].addEventListener("click" , ()=>{
+    FAQQuestionsContainer[i].classList.toggle("FAQQuestionsContainerHeight")
+    AnswerOfQuestion[i].classList.toggle("AnswerOfQuestionOpacity")
+    FAQOuterContainer[i].classList.toggle("FAQOuterContainerHeight")
 });
+console.log("hellp");
+}
