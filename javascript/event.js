949f752480b878eeec5fa6320db4f9191b049e6a
Raj varsani <varasaniraj5@gmail.com>
2021-06-19 15:45:27 +0530

    used include for events slider instead of code

--- javascript/event.js
+++ javascript/event.js
@@ -1,21 +1,24 @@
-var slider = document.getElementsByClassName("slide")
-let index_slider = 0;
-var isNotHovering = true
+setTimeout(function () {
+    var slider = document.getElementsByClassName("slide")
+    let index_slider = 0;
+    var isNotHovering = true
 
-var a = setInterval(() => {
-    if (isNotHovering) {
-        slider[index_slider].checked = true;
-        index_slider++;
-        index_slider = index_slider % 5;
-    }
-}, 2500)
+    var a = setInterval(() => {
+        if (isNotHovering) {
+            slider[index_slider].checked = true;
+            index_slider++;
+            index_slider = index_slider % 5;
+        }
+    }, 2500)
 
-var slide = document.getElementById("slider");
-slide.addEventListener('mouseover', () => {
-    console.log('hello')
-    isNotHovering = false
-})
+    var slide = document.getElementById("slider");
+    slide.addEventListener('mouseover', () => {
+        console.log('hello')
+        isNotHovering = false
+    })
 
-slide.addEventListener('mouseout', () => {
-    isNotHovering = true
-})
+    slide.addEventListener('mouseout', () => {
+        isNotHovering = true
+    })
+
+}, 200);
