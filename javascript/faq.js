be76e1df8c7a14c3575131440a85c0038d696bf7
HarshilMendpara <mendparaharshil@gmail.com>
2021-06-20 16:07:48 +0530

    resolving timeout error

--- javascript/faq.js
+++ javascript/faq.js
@@ -1,21 +1,18 @@
-
 setTimeout(function () {
+  let faq_down_arrow = document.getElementsByClassName("faq_down_arrow");
+  let FAQQuestionsContainer = document.getElementsByClassName(
+    "FAQQuestionsContainer"
+  );
+  let AnswerOfQuestion = document.getElementsByClassName("AnswerOfQuestion");
+  let arrowDiv = document.getElementsByClassName("arrowDiv");
+  let FAQOuterContainer = document.getElementsByClassName("FAQOuterContainer");
 
-    let faq_down_arrow = document.getElementsByClassName("faq_down_arrow");
-    let FAQQuestionsContainer = document.getElementsByClassName("FAQQuestionsContainer");
-    let AnswerOfQuestion = document.getElementsByClassName("AnswerOfQuestion");
-    let arrowDiv = document.getElementsByClassName("arrowDiv");
-    let FAQOuterContainer = document.getElementsByClassName("FAQOuterContainer");
-
-    for (let i = 0; i < faq_down_arrow.length; i++) {
-        faq_down_arrow[i].addEventListener("click", () => {
-            faq_down_arrow[i].classList.toggle("arrowDivRotate")
-            FAQQuestionsContainer[i].classList.toggle("FAQQuestionsContainerHeight")
-            AnswerOfQuestion[i].classList.toggle("AnswerOfQuestionOpacity")
-            FAQOuterContainer[i].classList.toggle("FAQOuterContainerHeight")
-        });
-        console.log("hello");
-    }
-
-
+  for (let i = 0; i < faq_down_arrow.length; i++) {
+    faq_down_arrow[i].addEventListener("click", () => {
+      faq_down_arrow[i].classList.toggle("arrowDivRotate");
+      FAQQuestionsContainer[i].classList.toggle("FAQQuestionsContainerHeight");
+      AnswerOfQuestion[i].classList.toggle("AnswerOfQuestionOpacity");
+      FAQOuterContainer[i].classList.toggle("FAQOuterContainerHeight");
+    });
+  }
 }, 200);
