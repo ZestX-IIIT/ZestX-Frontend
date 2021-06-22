1472f915482bba6833f565639c26a8e99a6123c8
HarshilMendpara <mendparaharshil@gmail.com>
2021-06-22 17:44:42 +0530

    added preloader in admin main page

--- javascript/admin_main_page.js
+++ javascript/admin_main_page.js
@@ -2,152 +2,183 @@ const apiURL = "https://whispering-ridge-40670.herokuapp.com";
 let token = localStorage.getItem("jwt");
 
 let ongoingEventContainer = document.getElementById(
-  "container_common_background"
+    "container_common_background"
 );
 let eventListArray = document.getElementsByClassName(
-  "primary_event_list_admin_page"
+    "primary_event_list_admin_page"
 );
+
+let adminMainPage = document.getElementById("admin_main_page_container");
+let preloader = document.getElementById("admin_preloader_container");
+let adminAddUserPage = document.getElementById("admin_add_user_page_container");
 let internalUserContainer = document.getElementById("internal_user_list");
 let externalUserContainer = document.getElementById("external_user_list");
+let addUserBtn = document.getElementById("secondh2");
 
 let ongoingEvents;
+let isFirstTime = true;
+
+addUserBtn.addEventListener("click", () => {
+    displayAdminAddUserPage();
+})
 
 fetch(`${apiURL}/fest/ongoingevents`, {
-  method: "GET",
+    method: "GET",
 })
-  .then((res) => res.json())
-  .then((data) => {
-    ongoingEvents = data.data;
+    .then((res) => res.json())
+    .then((data) => {
+        ongoingEvents = data.data;
 
-    setOngoingEvents(ongoingEvents);
-    setEventDetails(ongoingEvents[0]);
-    setUserDetails(ongoingEvents[0].user_id, ongoingEvents[0].external_user_id);
+        setOngoingEvents(ongoingEvents);
+        setEventDetails(ongoingEvents[0]);
+        setUserDetails(ongoingEvents[0].user_id, ongoingEvents[0].external_user_id);
 
-    for (let eventInstance of eventListArray) {
-      eventInstance.addEventListener("click", () => {
-        const event = ongoingEvents.find(
-          (item) => item.fest_id == eventInstance.id
-        );
-        setEventDetails(event);
-        setUserDetails(event.user_id, event.external_user_id);
-      });
-    }
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+        for (let eventInstance of eventListArray) {
+            eventInstance.addEventListener("click", () => {
+                checkAndCloseContainer(internalUserContainer);
+                checkAndCloseContainer(externalUserContainer);
+                const event = ongoingEvents.find(
+                    (item) => item.fest_id == eventInstance.id
+                );
+                setEventDetails(event);
+                setUserDetails(event.user_id, event.external_user_id);
+            });
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 function setOngoingEvents(array) {
-  ongoingEventContainer.innerHTML = "";
+    ongoingEventContainer.innerHTML = "";
 
-  array.forEach((item) => {
-    const { fest_id, fest_name, start_date, end_date } = item;
+    array.forEach((item) => {
+        const { fest_id, fest_name, start_date, end_date } = item;
 
-    var sDate = new Date(parseInt(start_date));
-    var eDate = new Date(parseInt(end_date));
-    let startDate =
-      sDate.getDate() +
-      "-" +
-      (sDate.getMonth() + 1) +
-      "-" +
-      sDate.getFullYear();
-    let endDate =
-      eDate.getDate() +
-      "-" +
-      (eDate.getMonth() + 1) +
-      "-" +
-      eDate.getFullYear();
+        var sDate = new Date(parseInt(start_date));
+        var eDate = new Date(parseInt(end_date));
+        let startDate =
+            sDate.getDate() +
+            "-" +
+            (sDate.getMonth() + 1) +
+            "-" +
+            sDate.getFullYear();
+        let endDate =
+            eDate.getDate() +
+            "-" +
+            (eDate.getMonth() + 1) +
+            "-" +
+            eDate.getFullYear();
 
-    const event = document.createElement("div");
-    event.classList.add("inner_list");
-    event.classList.add("primary_event_list_admin_page");
-    event.id = `${fest_id}`;
+        const event = document.createElement("div");
+        event.classList.add("inner_list");
+        event.classList.add("primary_event_list_admin_page");
+        event.id = `${fest_id}`;
 
-    const insideHtml = `<h3 class="innerList_h3">${fest_name}</h3>
+        const insideHtml = `<h3 class="innerList_h3">${fest_name}</h3>
     <h4 class="innerList_h4">${startDate} - ${endDate}</h4>
     `;
 
-    event.innerHTML = insideHtml;
+        event.innerHTML = insideHtml;
 
-    ongoingEventContainer.appendChild(event);
-  });
+        ongoingEventContainer.appendChild(event);
+    });
 }
 
 function setEventDetails(data) {
-  let rules = document.getElementById("RulesContentinnerList");
-  let description = document.getElementById("DescriptionContentinnerList");
-  let prizes = document.getElementById("PrizesContentinnerList");
+    let rules = document.getElementById("RulesContentinnerList");
+    let description = document.getElementById("DescriptionContentinnerList");
+    let prizes = document.getElementById("PrizesContentinnerList");
 
-  rules.innerHTML = `${data.rules}`;
-  description.innerHTML = `${data.description}`;
-  prizes.innerHTML = `${data.prize}`;
+    rules.innerHTML = `${data.rules}`;
+    description.innerHTML = `${data.description}`;
+    prizes.innerHTML = `${data.prize}`;
 }
 
 function setUserDetails(array1, array2) {
-  if (array1.length != 0) {
-    fetch(`${apiURL}/user/userdetails`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: token,
-      },
-      body: JSON.stringify({ ids: array1.toString() }),
-    })
-      .then(function (res) {
-        if (res.status == 400) {
-          alert("You have not access to admin panel!");
-        } else if (res.status == 500) {
-          alert("Internal server error please re-try!");
+    if (array1.length != 0) {
+        fetch(`${apiURL}/user/userdetails`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: token,
+            },
+            body: JSON.stringify({ ids: array1.toString() }),
+        })
+            .then(function (res) {
+                if (res.status == 400) {
+                    alert("You have not access to admin panel!");
+                } else if (res.status == 500) {
+                    alert("Internal server error please re-try!");
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setInternalUserDetails(data.data);
+                checkAndDisplayContainer(internalUserContainer);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    } else {
+        internalUserContainer.innerHTML = "";
+        checkAndDisplayContainer(internalUserContainer);
+    }
+    if (array2 != null) {
+        fetch(`${apiURL}/user/exuserdetails`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: token,
+            },
+            body: JSON.stringify({ ids: array2.toString() }),
+        })
+            .then(function (res) {
+                if (res.status == 400) {
+                    alert("You have not access to admin panel!");
+                } else if (res.status == 500) {
+                    alert("Internal server error please re-try!");
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setExternalUserDetails(data.data);
+                if (isFirstTime) {
+                    setTimeout(function () {
+                        displayAdminMainpage();
+                    }, 100);
+                    isFirstTime = false;
+                } else {
+                    checkAndDisplayContainer(externalUserContainer);
+                }
+
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    } else {
+        externalUserContainer.innerHTML = "";
+        if (isFirstTime) {
+            setTimeout(function () {
+                displayAdminMainpage();
+            }, 100);
+            isFirstTime = false;
+        } else {
+            checkAndDisplayContainer(externalUserContainer);
         }
-        return res.json();
-      })
-      .then((data) => {
-        setInternalUserDetails(data.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  } else {
-    internalUserContainer.innerHTML = "";
-  }
-  if (array2 != null) {
-    fetch(`${apiURL}/user/exuserdetails`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: token,
-      },
-      body: JSON.stringify({ ids: array2.toString() }),
-    })
-      .then(function (res) {
-        if (res.status == 400) {
-          alert("You have not access to admin panel!");
-        } else if (res.status == 500) {
-          alert("Internal server error please re-try!");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setExternalUserDetails(data.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  } else {
-    externalUserContainer.innerHTML = "";
-  }
+    }
 }
 
 function setInternalUserDetails(data) {
-  internalUserContainer.innerHTML = "";
+    internalUserContainer.innerHTML = "";
 
-  data.forEach((item) => {
-    const { user_name, email, mobile } = item;
+    data.forEach((item) => {
+        const { user_name, email, mobile } = item;
 
-    const user = document.createElement("div");
-    user.classList.add("inner_list_content");
+        const user = document.createElement("div");
+        user.classList.add("inner_list_content");
 
-    const insideHtml = `<div class="inner_list">
+        const insideHtml = `<div class="inner_list">
     <div class="inner_list_content">
         <h3 class="innerList_h3" id="user_name">${user_name}</h3>
         <h4 class="innerList_h4" id="user_details">${email} || ${mobile}</h4>
@@ -156,32 +187,87 @@ function setInternalUserDetails(data) {
 </div>
     `;
 
-    user.innerHTML = insideHtml;
+        user.innerHTML = insideHtml;
 
-    internalUserContainer.appendChild(user);
-  });
+        internalUserContainer.appendChild(user);
+    });
 }
 
 function setExternalUserDetails(data) {
-  externalUserContainer.innerHTML = "";
+    externalUserContainer.innerHTML = "";
 
-  data.forEach((item) => {
-    const { username, email, mobile } = item;
+    data.forEach((item) => {
+        const { username, email, mobile } = item;
 
-    const user = document.createElement("div");
-    user.classList.add("inner_list_content");
+        const user = document.createElement("div");
+        user.classList.add("inner_list_content");
+        let insideHtml;
 
-    const insideHtml = `<div class="inner_list">
-    <div class="inner_list_content">
-        <h3 class="innerList_h3" id="user_name">${username}</h3>
-        <h4 class="innerList_h4" id="user_details">${email} || ${mobile}</h4>
+        if (email && mobile) {
+            insideHtml = `<div class="inner_list">
+        <div class="inner_list_content">
+            <h3 class="innerList_h3" id="user_name">${username}</h3>
+            <h4 class="innerList_h4" id="user_details">${email} || ${mobile}</h4>
+        </div>
+        <img src="../assets/home_page/Admin_page/delete_user_btn.svg" alt="">
     </div>
-    <img src="../assets/home_page/Admin_page/delete_user_btn.svg" alt="">
-</div>
-    `;
+        `;
+        } else {
+            insideHtml = `<div class="inner_list">
+        <div class="inner_list_content">
+            <h3 class="innerList_h3" id="user_name">${username}</h3>
+            <h4 class="innerList_h4" id="user_details">${email} ${mobile}</h4>
+        </div>
+        <img src="../assets/home_page/Admin_page/delete_user_btn.svg" alt="">
+    </div>
+        `;
+        }
 
-    user.innerHTML = insideHtml;
+        user.innerHTML = insideHtml;
 
-    externalUserContainer.appendChild(user);
-  });
+        externalUserContainer.appendChild(user);
+    });
+}
+
+function displayAdminMainpage() {
+    checkAndCloseContainer(preloader);
+    checkAndDisplayContainer(adminMainPage);
+    checkAndCloseContainer(adminAddUserPage);
+}
+
+function displayPreloder() {
+    checkAndDisplayContainer(preloader);
+    checkAndCloseContainer(adminMainPage);
+    checkAndCloseContainer(adminAddUserPage);
+}
+
+function displayAdminAddUserPage() {
+    checkAndDisplayContainer(adminAddUserPage);
+    checkAndCloseContainer(adminMainPage);
+    checkAndCloseContainer(preloader);
+}
+
+function checkAndCloseContainer(container) {
+    if (container.classList.contains("display_to_block")) {
+        container.classList.remove("display_to_block");
+    }
+    container.classList.add("display_to_none");
+
+    if (container.style.display != "none") {
+        container.classList.add("display_to_none");
+
+        setTimeout(function () {
+            container.style.display = "none";
+            container.style.opacity = 0;
+        }, 500);
+    }
+}
+
+function checkAndDisplayContainer(container) {
+    container.style.display = "block";
+    container.style.opacity = 1;
+    if (container.classList.contains("display_to_none")) {
+        container.classList.remove("display_to_none");
+    }
+    container.classList.add("display_to_block");
 }
