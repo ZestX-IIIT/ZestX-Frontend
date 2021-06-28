2fbe5194661d692ab91ed6eef37fd19e51c91600
HarshilMendpara <mendparaharshil@gmail.com>
2021-06-28 18:34:55 +0530

    added toast to homepage

--- javascript/toast.js
+++ javascript/toast.js
@@ -1,25 +1,34 @@
-let toastAlertMessage = document.getElementById("toastAlertMessage");
-let toastImage = document.getElementById("toastImage");
-let toastFrontMessage = document.getElementById("toastFrontMessage");
-let toastDescriptionMessage = document.getElementById("toastDescriptionMessage");
-let isSuccess , message;
+setTimeout(function () {
 
-function show_toast(isSuccess , message) {
-    if(isSuccess == 1){
-        toastImage.src = "../assets/_general/success_tick.svg"
-        toastFrontMessage.style.backgroundColor = "green"
+  let toastAlertMessage = document.getElementById("toastAlertMessage");
+  let toastImage = document.getElementById("toastImage");
+  let toastFrontMessage = document.getElementById("toastFrontMessage");
+  let toastDescriptionMessage = document.getElementById("toastDescriptionMessage");
+
+  console.log("hello");
+  function show_toast(isSuccess, message) {
+    if (isSuccess == 1) {
+      toastImage.src = "../assets/_general/success_tick.svg"
+      toastFrontMessage.style.backgroundColor = "green"
     }
-    else if(isSuccess == 0){
-        toastImage.src = "../assets/_general/error_cross.svg"
-        toastFrontMessage.style.backgroundColor = "red"
+    else if (isSuccess == 0) {
+      toastImage.src = "../assets/_general/error_cross.svg"
+      toastFrontMessage.style.backgroundColor = "red"
     }
-    else{
+    else {
       toastImage.src = "../assets/_general/neutral_exclamation.svg"
       toastFrontMessage.style.backgroundColor = "black"
     }
-    toastDescriptionMessage.innerText = message;
+
+    toastDescriptionMessage.innerText = " ";
+    setTimeout(function () {
+      toastDescriptionMessage.innerText = message;
+    }, 2000);
+
     toastAlertMessage.className = "toastPopUp";
-  setTimeout(function () {
-    toastAlertMessage.className = toastAlertMessage.className.replace("toastPopUp", "");
-  }, 5000);
-}
+    setTimeout(function () {
+      toastAlertMessage.className = toastAlertMessage.className.replace("toastPopUp", "");
+    }, 5000);
+  }
+
+}, 200);
