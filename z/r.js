3e2c71d865588381d9cddb5cc9b98db4a87589c6
Raj varsani <varasaniraj5@gmail.com>
2021-06-29 15:10:54 +0530

    updated scrollbar activation area

--- z/r.js
+++ z/r.js
@@ -2,9 +2,17 @@ let progress = document.getElementById("Progressbar");
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 let windowWidth = window.innerWidth;
 let limit = windowWidth - 100;
+let limit2 = windowWidth - 20;
+let coloer_grediet_value_1;
+let coloer_grediet_value_2;
 window.onscroll = function () {
   let progressHeight = (window.pageYOffset / totalHeight) * 100;
   progress.style.height = progressHeight + "%";
+
+  coloer_grediet_value_1 = (210 - 150 * window.pageYOffset / totalHeight) + "," + (14  + 250)  + "," + 236
+  coloer_grediet_value_2 = 11 + "," + 189 + "," + 202
+  progress.style.background = "linear-gradient(to top, rgb(" + coloer_grediet_value_1 + "), rgb(" + coloer_grediet_value_2 + "))";
+
 };
 var y = 0;
 let scrollPath = document.getElementById("scrollPath");
@@ -19,8 +27,13 @@ console.log(timeTemp);
 console.log(scrollPath2);
 
 function startProgressbarScroll(e) {
+
+  if (e.clientX > limit2) {
   isProgressbarActive = true;
   console.log("true")
+  progressBarScroll(e)
+  }
+
 }
 function stopProgressbarScroll(e) {
   isProgressbarActive = false;
@@ -28,16 +41,15 @@ function stopProgressbarScroll(e) {
 }
 
 function progressBarScroll(e) {
-  // console.log(Date.now() -timeTemp)
-  // if (Date.now() - timeTemp > 500) {
-    if (isProgressbarActive) {
-      if (e.clientX > limit) {
-        y = e.clientY;
-        window.scrollBy(0, (((y * totalHeight) / window.innerHeight) - window.pageYOffset)/10 );
-
-      }
+  // console.log(Date.now() - timeTemp)
+  // if (Date.now() - timeTemp > 200) {
+  if (isProgressbarActive) {
+    if (e.clientX > limit) {
+      y = e.clientY;
+      window.scrollBy(0, (((y * totalHeight) / window.innerHeight) - window.pageYOffset) / 10);
     }
-  //   timeTemp= Date.now()
+  }
+  // timeTemp = Date.now()
   // }
 
 }
