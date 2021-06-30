5f53430796fd53c48d8314148a97b0a0ec8af637
Raj varsani <varasaniraj5@gmail.com>
2021-06-30 20:41:25 +0530

    updated hovers in team section

--- javascript/team.js
+++ javascript/team.js
@@ -1,29 +1,56 @@
-// setTimeout(()=>{
-    
+
 let imageHolder = document.getElementsByClassName("imageHolder");
 let outerContainer = document.getElementsByClassName("outerContainer");
-let team_section_hover_icons_not_hover_github = document.getElementsByClassName("team_section_hover_icons_not_hover_github");
-let team_section_hover_icons_not_hover_linkedin = document.getElementsByClassName("team_section_hover_icons_not_hover_linkedin");
-let team_section_hover_icons_not_hover_mail = document.getElementsByClassName("team_section_mail");
-let team_section_hover_icons_under_hover_github = document.getElementsByClassName("team_section_hover_icons_under_hover_github")
-for(let i=0; i<imageHolder.length;i++){
-    outerContainer[i].addEventListener("mouseenter" , ()=>{
+
+let github_icons = document.getElementsByClassName("team_section_github");
+let linkedin_icons = document.getElementsByClassName("team_section_linkedin");
+let mail_icons = document.getElementsByClassName("team_section_mail");
+
+// let team_section_hover_icons_not_hover_github = document.getElementsByClassName("team_section_hover_icons_not_hover_github");
+// let team_section_hover_icons_not_hover_linkedin = document.getElementsByClassName("team_section_hover_icons_not_hover_linkedin");
+// let team_section_hover_icons_not_hover_mail = document.getElementsByClassName("team_section_mail");
+// let team_section_hover_icons_under_hover_github = document.getElementsByClassName("team_section_hover_icons_under_hover_github")
+
+console.log(github_icons);
+
+for (let i = 0; i < imageHolder.length; i++) {
+    outerContainer[i].addEventListener("mouseenter", () => {
         imageHolder[i].classList.add("imageHolder_toggle");
-        team_section_hover_icons_not_hover_mail[i].style.opacity = 1;
-        team_section_hover_icons_not_hover_github[i].style.opacity = 1;
-        team_section_hover_icons_not_hover_linkedin[i].style.opacity = 1;
+        mail_icons[2*i].style.opacity = 1;
+        github_icons[2*i].style.opacity = 1;
+        linkedin_icons[2*i].style.opacity = 1;
     })
-    outerContainer[i].addEventListener("mouseleave" , ()=>{
+    outerContainer[i].addEventListener("mouseleave", () => {
         imageHolder[i].classList.remove("imageHolder_toggle");
-        team_section_hover_icons_not_hover_mail[i].style.opacity = 0;
-        team_section_hover_icons_not_hover_github[i].style.opacity = 0;
-        team_section_hover_icons_not_hover_linkedin[i].style.opacity = 0;
-    })
-    
-    team_section_hover_icons_not_hover_github[i].addEventListener("mouseover" , ()=>{
-        // this.style.opacity = 0;
-        // team_section_hover_icons_under_hover_github[i].style.opacity = 1;
-        console.log("hello");
+        mail_icons[2*i].style.opacity = 0;
+        github_icons[2*i].style.opacity = 0;
+        linkedin_icons[2*i].style.opacity = 0;
     })
 }
-// },200);
+
+for (let i = 0; i < github_icons.length; i=i+2) {
+    github_icons[i+1].addEventListener("mouseenter", () => {
+        github_icons[i+1].style.opacity = 1;
+        github_icons[i].style.opacity = 0;
+    })
+    github_icons[i+1].addEventListener("mouseleave", () => {
+        github_icons[i+1].style.opacity = 0;
+        github_icons[i].style.opacity = 1;
+    })
+    mail_icons[i+1].addEventListener("mouseenter", () => {
+        mail_icons[i+1].style.opacity = 1;
+        mail_icons[i].style.opacity = 0;
+    })
+    mail_icons[i+1].addEventListener("mouseleave", () => {
+        mail_icons[i+1].style.opacity = 0;
+        mail_icons[i].style.opacity = 1;
+    })
+    linkedin_icons[i+1].addEventListener("mouseenter", () => {
+        linkedin_icons[i+1].style.opacity = 1;
+        linkedin_icons[i].style.opacity = 0;
+    })
+    linkedin_icons[i+1].addEventListener("mouseleave", () => {
+        linkedin_icons[i+1].style.opacity = 0;
+        linkedin_icons[i].style.opacity = 1;
+    })
+}
