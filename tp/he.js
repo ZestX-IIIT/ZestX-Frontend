6ef229b1dcda3c8d7c62900fdc8895b2d80fb3ed
Raj varsani <varasaniraj5@gmail.com>
2021-06-30 20:21:38 +0530

    checking lottie

---
+++ tp/he.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('hehe'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'heheheheh.json'
+  })
