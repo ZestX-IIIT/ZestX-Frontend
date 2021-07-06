bb7e4e2b15bef6ff6af29a0740b4e0c65c889dec
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-06 23:30:00 +0530

    created js to validate password

---
+++ javascript/password_validator.js
@@ -0,0 +1,44 @@
+let regularExpression = /^(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/;
+
+function passValidator(password) {
+    let boolValue = false;
+    let message;
+
+    if (password.length < 6) {
+        message = 'Password should be minimum of 6 length!';
+        return [boolValue, message];
+    }
+
+    if (password.length > 20) {
+        message = 'Password should be maximum of 20 length!';
+        return [boolValue, message];
+    }
+
+    if (password.search(/[A-Z]/) < 0) {
+        message = 'Password should contain atleast 1 uppercase letter!';
+        return [boolValue, message];
+    }
+
+    if (password.search(/[a-z]/) < 0) {
+        message = 'Password should contain atleast 1 lowercase letter!';
+        return [boolValue, message];
+    }
+
+    if (password.search(/[0-9]/) < 0) {
+        message = 'Password should contain atleast 1 digit!';
+        return [boolValue, message];
+    }
+
+    if (password.search(' ') != -1) {
+        message = 'Password should not contain any spaces!';
+        return [boolValue, message];
+    }
+
+    if (!regularExpression.test(password)) {
+        message = 'Password should contain atleast 1 special character!';
+        return [boolValue, message];
+    }
+
+    return [true, "Success"];
+
+}
