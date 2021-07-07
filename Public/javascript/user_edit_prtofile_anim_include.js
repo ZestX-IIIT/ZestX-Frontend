efc0924c340ab7bd8b9fa32a7456e6414ec17e91
Akash Gupta <akash.gupta.anup@gmail.com>
2021-07-07 14:01:25 +0530

    removed public

--- Public/javascript/user_edit_prtofile_anim_include.js
+++
@@ -1,7 +0,0 @@
-    var adminAddUserAnimation = bodymovin.loadAnimation({
-        container: document.getElementById('user_edit_profile_anim_container'),
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: '../anim/user_edit_profile_page_anim/user_edit_profile_page_anim.json'
-    })
