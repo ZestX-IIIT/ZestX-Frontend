185f780690fbe648032e9c9ae4df9379514b9f59
Raj varsani <varasaniraj5@gmail.com>
2021-07-08 15:20:42 +0530

    Updated edit profile animation path

--- javascript/user_edit_prtofile_anim_include.js
+++ javascript/user_edit_prtofile_anim_include.js
@@ -1,7 +1,7 @@
-    var adminAddUserAnimation = bodymovin.loadAnimation({
-        container: document.getElementById('user_edit_profile_anim_container'),
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: '../anim/user_edit_profile_page_anim/user_edit_profile_page_anim.json'
-    })
+var user_edit_profile_anim = bodymovin.loadAnimation({
+    container: document.getElementById('user_edit_profile_anim_container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../anim/user_edit_profile_page_anim/high_res/user_edit_profile_page_anim.json'
+})
