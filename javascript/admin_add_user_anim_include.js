e8eb5e29105f928a887c50b829df2e957722e9ea
Raj varsani <varasaniraj5@gmail.com>
2021-07-08 16:16:43 +0530

    Updated add use page anim include path

--- javascript/admin_add_user_anim_include.js
+++ javascript/admin_add_user_anim_include.js
@@ -1,13 +1,7 @@
-// setTimeout(function () {
-
-    var adminAddUserAnimation = bodymovin.loadAnimation({
-        container: document.getElementById('admin_add_user_anim_container'),
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: '../anim/admin_add_user_page_anim/admin_add_user_page_anim.json'
-    })
-
-// }, 500);
-
-
+var adminAddUserAnimation = bodymovin.loadAnimation({
+    container: document.getElementById('admin_add_user_anim_container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../anim/admin_add_user_page_anim/high_res/admin_add_user_page_anim.json'
+})
