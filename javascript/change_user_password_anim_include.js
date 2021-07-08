802b63a4e50c8a6d0a5b099e69ad7a8d5dafde2d
Raj varsani <varasaniraj5@gmail.com>
2021-07-08 15:22:11 +0530

    Updated animaton name

--- javascript/change_user_password_anim_include.js
+++ javascript/change_user_password_anim_include.js
@@ -1,7 +1,7 @@
-    var adminAddUserAnimation = bodymovin.loadAnimation({
-        container: document.getElementById('change_user_password_page_anim_container'),
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: '../anim/security_anim_edit_password_page/high-res/security_anim_data.json'
-    })
+var chnageUserPasswordAnim = bodymovin.loadAnimation({
+    container: document.getElementById('change_user_password_page_anim_container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../anim/security_anim_edit_password_page/high-res/security_anim_data.json'
+})
