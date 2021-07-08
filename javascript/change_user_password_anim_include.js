164843fc3425a89c0daf1989de9072e30e2c3b78
Raj varsani <varasaniraj5@gmail.com>
2021-07-08 15:09:34 +0530

    Updated path of anim in change opassword page

--- javascript/change_user_password_anim_include.js
+++ javascript/change_user_password_anim_include.js
@@ -3,7 +3,5 @@
         renderer: 'svg',
         loop: true,
         autoplay: true,
-        path: '../anim/security_anim_edit_password_page/security_anim_data.json'
+        path: '../anim/security_anim_edit_password_page/high-res/security_anim_data.json'
     })
-
-
