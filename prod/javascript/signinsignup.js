45d0d03faea988b61ad0f0079335df6b19a163fa
Raj varsani <varasaniraj5@gmail.com>
2021-07-08 14:08:23 +0530

    Updated prod files

--- prod/javascript/signinsignup.js
+++ prod/javascript/signinsignup.js
@@ -17,7 +17,7 @@ let lastToastTimestamp = Date.now();
 const apiURL = "https://whispering-ridge-40670.herokuapp.com";
 
 setTimeout(() => {
-  setUpSignInSignUpPage()
+  redirect(0 ,setUpSignInSignUpPage)
 }, 200);
 
 function setUpSignInSignUpPage() {
@@ -235,6 +235,72 @@ function setUpSignInSignUpPage() {
     duration: 600,
   });
 }
+
+var redirect = function (redirectWithoutTokenChk, setUpFun) {
+
+  const token = localStorage.getItem("jwt");
+  if (token && token != null) {
+
+      fetch(`https://whispering-ridge-40670.herokuapp.com/user/getdetails`, {
+          method: "GET",
+          headers: {
+              authorization: token,
+          },
+      })
+          .then((res) => res.json())
+          .then((data) => {
+              let isAdmin = data.data.is_admin;
+              switch (redirectWithoutTokenChk) {
+                  case 0:
+                      redirectToHomeOrAdminPageAccToToken(isAdmin);
+                      break;
+                  case 1:
+                      redirectToHomePageAccToToken(isAdmin, setUpFun);
+                      break;
+                  case 2:
+                      redirectToAdminPageAccToToken(isAdmin, setUpFun);
+                      break;
+
+                  default:
+                      break;
+              }
+          })
+          .catch((err) => {
+              console.log(err);
+          });
+
+
+  } else {
+      if (redirectWithoutTokenChk > 0) {
+          window.location.href = "../signupsignin.html";
+      }
+      else {
+          setUpFun()
+
+      }
+  }
+}
+
+function redirectToHomeOrAdminPageAccToToken(isAdmin) {
+  if (isAdmin)
+      window.location.href = "./general/admin_main_page.html";
+  else window.location.href = "./homepage.html";
+}
+
+function redirectToHomePageAccToToken(isAdmin, setUpFun) {
+  if (!isAdmin)
+      window.location.href = "../homepage.html";
+  else
+      setUpFun()
+}
+
+function redirectToAdminPageAccToToken(isAdmin, setUpFun) {
+  if (isAdmin)
+      window.location.href = "./general/admin_main_page.html";
+  else
+      setUpFun()
+}
+
 
 function show_toast(isSuccess, message) {
 
