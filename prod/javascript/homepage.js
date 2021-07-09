cc8906b8fce997ad46481517614e615f1310f6a6
Raj varsani <varasaniraj5@gmail.com>
2021-07-09 15:47:13 +0530

    Updated paths in homepage js

--- prod/javascript/homepage.js
+++ prod/javascript/homepage.js
@@ -2122,14 +2122,14 @@ setTimeout(() => {
         renderer: 'svg',
         loop: true,
         autoplay: true,
-        path: '/user_edit_profile_page_anim/high_res/user_edit_profile_page_anim.json'
+        path: './anim/user_edit_profile_page_anim/high_res/user_edit_profile_page_anim.json'
     })
     var userChangePasswordAnimation = bodymovin.loadAnimation({
         container: document.getElementById('change_user_password_page_anim_container'),
         renderer: 'svg',
         loop: true,
         autoplay: true,
-        path: '/security_anim_edit_password_page/high-res/security_anim_data.json'
+        path: './anim/security_anim_edit_password_page/high-res/security_anim_data.json'
     })
 
 }, 500);
